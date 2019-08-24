3072568288e1c484f05bb45056b667ae5a96abb2
Anthony McPherson <aqm28202@gmail.com>
2019-08-24 19:36:41 -0400

    <wired submit button to alert, as a test to make sure its working>

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
     
 // Before you can make any part of your site work, you need to create an array of strings, each one related to a topic that interests you. Save it to a variable called topics.
-    var topics = ["Coming to America", "Bridesmaids", "Meet the Parents", "Friday", "Wedding Crashers", "The Nutty Professor", "Hitch", "Death at a Funeral" ]
+    
+var topics = ["Coming to America", "Bridesmaids", "Meet the Parents", "Friday", "Wedding Crashers", "The Nutty Professor", "Hitch", "Death at a Funeral" ]
 
 // Your app should take the topics in this array and create buttons in your HTML. Try using a loop that appends a button for each string in the array.
+
 function renderButtons() {
     for (var i=0; i < topics.length; i++) {
         var movieBtn = $("<button>");
@@ -16,3 +18,10 @@ function renderButtons() {
 }
 
 renderButtons()
+
+// When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
+
+//this function handles events where one button is clicked
+$("#submit").on('click', function() {
+    alert("I have been clicked");
+});
