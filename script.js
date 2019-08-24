2e161711c11c1bfdb64564cfb30d11bd45bc9f67
Anthony McPherson <aqm28202@gmail.com>
2019-08-24 18:36:32 -0400

    <built initial array to house movie titles>

---
+++ script.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    
+// Before you can make any part of your site work, you need to create an array of strings, each one related to a topic that interests you. Save it to a variable called topics.
+    var topics = ["Coming to America", "Bridesmaids", "Meet the Parents", "Friday", "Wedding Crashers", "The Nutty Professor", "Hitch", "Death at a Funeral" ]
