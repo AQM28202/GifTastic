4874614259203ef4746da8e0668d6d2607e7ad50
Anthony McPherson <aqm28202@gmail.com>
2019-08-24 18:44:46 -0400

    <I'm building a function with a for loop inside to render buttons. So far I just have the function name and the forloop>

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     var topics = ["Coming to America", "Bridesmaids", "Meet the Parents", "Friday", "Wedding Crashers", "The Nutty Professor", "Hitch", "Death at a Funeral" ]
 
 // Your app should take the topics in this array and create buttons in your HTML. Try using a loop that appends a button for each string in the array.
-
-
+function renderButtons() {
+    for (var i=0; i < topics.length; i++) {
 
 
