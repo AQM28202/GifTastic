9a879f26c436b4258128608962bbcb2ded90b2de
Anthony McPherson <aqm28202@gmail.com>
2019-08-24 20:21:11 -0400

    <Creating variable for the new button that is created by user input AND Creating variable to house query url>

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 
     
 // Before you can make any part of your site work, you need to create an array of strings, each one related to a topic that interests you. Save it to a variable called topics.
-    
 var topics = ["Coming to America", "Bridesmaids", "Meet the Parents", "Friday", "Wedding Crashers", "The Nutty Professor", "Hitch", "Death at a Funeral" ]
 
 // Your app should take the topics in this array and create buttons in your HTML. Try using a loop that appends a button for each string in the array.
-
 function renderButtons() {
     for (var i=0; i < topics.length; i++) {
         var movieBtn = $("<button>");
@@ -20,12 +18,10 @@ function renderButtons() {
 renderButtons()
 
 // When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
-
 $("#submit").on('click', function() {
     event.preventDefault();
 
 //Grabs the text from the input field
-
 var movie = $("#add-movie").val().trim();
 
 // Adds user input to array and creates new button
@@ -34,10 +30,14 @@ $("#buttons-display").empty();
 renderButtons()
 });
 
-// Wirimg up movie buttons to function when clicked
+// Wiring up movie buttons to function when clicked
 $(document).on('click', '.btn-outline-primary',  function() {
-    alert("I'm working");
+   
+// Creating variable for the new button that is created by user input
+ var newButton = $(this).attr('movie-name');
+
+ // Creating variable to house query url
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + newButton + "&api_key=HxjyPcOPD2gWbFGHpMzakdcqp1m6KWI9"; 
 
 });
 
-    // var newButton = $(this).attr('movie-name');
